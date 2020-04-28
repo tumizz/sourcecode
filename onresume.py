dd09d5c12e1f1558e07ff42fe509208fd489269e
loze <lozejs@gmail.com>
2020-04-29 01:26:48 +0900

    onresume_message

--- onresume.py
+++ onresume.py
@@ -1,17 +1,24 @@
 import frida, sys
 
+def on_message(message, data):
+    if message['type'] == 'send':
+        print(message['payload'])
+    else:
+        print(message)
+
 jscode = """
 setImmediate(function(){
 	Java.perform(function(){
 		var Activity=Java.use("android.app.Activity")
 		Activity.onResume.implementation = function(){
-			console.log("[*] onResume() got called!");
+			send("[*] onResume() got called!");
 			this.onResume();
 		};
 	});
 });
 """
-process = frida.get_usb_device().attach("com.android.chrome")
-script = process.create_script(jscode)
+session = frida.get_usb_device().attach("package.name")
+script = session.create_script(jscode)
+script.on('message', on_message)
 script.load()
 sys.stdin.read()
